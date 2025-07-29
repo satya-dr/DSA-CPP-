b9699ce8cdef604762d7d505653418203fb6a477
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-07-29 14:25:44 +0530

    Binary to decimal

--- 6th chapter/index.cpp
+++ 6th chapter/index.cpp
@@ -1,25 +1,51 @@
-//Decimal Number to Binary Number
+// //Decimal Number to Binary Number
+// #include<iostream>
+// using namespace std;
+// int decToBi(int n){
+//     int ans=0;
+//     int pow=1;//10^0=1
+//     while(n>0){
+//         int remender=n%2;
+//         n=n/2;
+//         ans+=(remender*pow);
+//         pow=pow*10;
+//     }
+//     return ans;
+// }
+// int main(){
+//     int n;
+//     cout<<"Enter a number : ";
+//     cin>>n;
+
+//     for(int i=1;i<=n;i++){
+
+//         cout<<"Binary of "<<i<< " is : "<<decToBi(i)<<endl;
+//     }
+
+// }
+
+
+//Binary to Decimal
 #include<iostream>
 using namespace std;
-int decToBi(int n){
+int bianryToDecimal(int num){
     int ans=0;
-    int pow=1;
-    while(n>0){
-        int remender=n%2;
-        n=n/2;
-        ans+=(remender*pow);
-        pow=pow*10;
+    int pow=1;//2^0=1
+    while(num>0){
+        int rem=num%10;
+        num/=10;
+        ans+=(rem*pow);
+        pow*=2;
     }
+
     return ans;
+
+
 }
 int main(){
     int n;
     cout<<"Enter a number : ";
     cin>>n;
 
-    for(int i=1;i<=n;i++){
-
-        cout<<"Binary of "<<i<< " is : "<<decToBi(i)<<endl;
-    }
-
+    cout<<"Decimal of "<<n<<" is : "<<bianryToDecimal(n);
 }

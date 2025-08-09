1e6fe82154c8cf9fc7d74d84b91e7726858b8281
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-08-10 01:26:53 +0530

    Array complete

--- 8th chapter/questions.cpp
+++ 8th chapter/questions.cpp
@@ -1,13 +1,63 @@
+
+// //Sum of all element of a array
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[10];
+//     int sum =0;
+//     int size=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<size;i++){
+//         cout<<"Enter "<<i+1<<" number: ";
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<size;i++){
+//         sum+=arr[i];
+//     }
+//     cout <<sum<<endl;
+
+
+// }
+
+// // Find the maximum value of all elements in the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter array size: ";
+//     cin>>n;
+//     int arr[n];
+//     int size=sizeof(arr)/sizeof(arr[0]);
+//     for(int i=0;i<size;i++){
+//         cout<<"Enter "<<i+1<<" number: ";
+//         cin>>arr[i];
+//     }
+//     int max=arr[0];
+//     for(int i=0;i<size;i++){
+//        if(arr[i]>=max){
+//         max=arr[i];
+//        }
+//     }
+//     cout <<max<<endl;
+
+
+// }
+
+
+
+// Linear search 
 #include<iostream>
 using namespace std;
 int main(){
-    int arr[]={2,3,4,5};
-    int sum =0;
-    int size=sizeof(arr)/sizeof(arr[0]);
-    for(int i=0;i<size;i++){
-        sum+=arr[i];
+
+    int arr[]={1,2,3,4,5};
+    int key=3;
+    int ans=-1;
+    for(int i=0;i<5;i++){
+        if(arr[i]==key){
+            ans=i;
+            break;
+        }
     }
-    cout <<sum<<endl;
-
-
+    cout<<ans+1;
 }
